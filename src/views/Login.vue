581aac502ca2020fa35911128619c1a8b386402c
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-07-16 09:22:00 +0200

    fix login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -17,16 +17,16 @@ export default {
 };
 </script>
 <template>
-    <div>
-    <h1>Login</h1>
-    <v-form @submit.prevent="login">
-      <v-text-field 
-        label="Nome Utente" 
-        v-model="username" 
-        required
-      ></v-text-field>
-      <v-btn type="submit" color="primary">Login</v-btn>
-    </v-form>
+  <h1>Login</h1>
+    <div class="w-50 ms-auto me-auto text-center" >
+      <v-form @submit.prevent="login">
+        <v-text-field 
+          label="Nome Utente" 
+          v-model="username" 
+          required
+        ></v-text-field>
+        <v-btn type="submit" color="primary">Login</v-btn>
+      </v-form>
     </div>
 </template>
 
