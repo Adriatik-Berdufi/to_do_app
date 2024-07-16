70a12564bf0cbca8b44a1ef40a2f3e98aafa3c61
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-07-16 09:41:45 +0200

    Add Axios and Albo page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,6 +7,7 @@
         <v-spacer></v-spacer>
         <v-btn text :to="{ name: 'Home' }">Home</v-btn>
         <v-btn text :to="{ name: 'Completed' }">Completed</v-btn>
+        <v-btn text :to="{ name: 'Albo' }">Albo</v-btn>
         <v-btn text :to="{ name: 'Login' }">Login</v-btn>
     </v-app-bar>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Completed from '../views/Completed.vue';
 import Login from '../views/Login.vue';
+import Albo from '../views/Albo.vue';
 
 const routes = [
   { path: '/', name: 'Login', component: Login },
   { path: '/home', name: 'Home', component: Home },
   { path: '/completed', name: 'Completed', component: Completed },
+  { path: '/albo', name: 'Albo', component: Albo },
 ];
 
 const router = createRouter({
